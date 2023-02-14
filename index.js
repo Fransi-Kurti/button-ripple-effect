a6d8fb121183fb31eae266b3c536a93f652173c0
fransi-kurti <fransi.kurti@gmail.com>
2023-02-14 19:56:35 +0100

    button ripple effect (finished)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btnEl = document.getElementById("btn");
 
-btnEl.addEventListener("mouseover", (event)=>{
+btnEl.addEventListener("mouseover", ()=>{
     const x = event.pageX - btnEl.offsetLeft;
     const y = event.pageY - btnEl.offsetTop;
     btnEl.style.setProperty("--positionX", x + "px");
